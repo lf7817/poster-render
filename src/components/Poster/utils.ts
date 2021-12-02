595aff6dec6c01375661a525ff1e33244307f38e
lf7817 <535536456@qq.com>
2021-12-02 20:19:45 +0800

    fix: 修改topx

--- src/components/Poster/utils.ts
+++ src/components/Poster/utils.ts
@@ -1,19 +1,14 @@
-import Taro from "@tarojs/taro";
+import Taro from '@tarojs/taro';
 
-export const delay = (time: number) =>
-  new Promise((resolve) => setTimeout(resolve, time));
+export const delay = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
 
 export const { screenWidth, pixelRatio } = Taro.getSystemInfoSync();
-export const isAlipay = Taro.getEnv() === "ALIPAY";
+export const isAlipay = Taro.getEnv() === 'ALIPAY';
 export const factor = screenWidth / 750;
 
 export function toPx(rpx: number) {
-  return isAlipay
-    ? Math.ceil(pixelRatio * rpx * factor)
-    : Math.ceil(factor * rpx);
+  return isAlipay ? Math.round(pixelRatio * rpx * factor) : Math.round(factor * rpx);
 }
 export function toRpx(px: number) {
-  return isAlipay
-    ? Math.ceil(px / pixelRatio / factor)
-    : Math.round(px / factor);
+  return isAlipay ? Math.round(px / pixelRatio / factor) : Math.round(px / factor);
 }
